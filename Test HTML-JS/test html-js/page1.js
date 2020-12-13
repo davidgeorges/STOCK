b69d3058450e4aabf0b2439e95fd9f6cf9c699c1
David <69092875+davidgeorges@users.noreply.github.com>
2020-12-13 16:17:49 +0100

    Commit New

--- Test HTML-JS/test html-js/page1.js
+++ Test HTML-JS/test html-js/page1.js
@@ -15,6 +15,7 @@ function bouton1(){
         myElement.innerHTML = nb1;
         // on écrit a la balise b1 la valeur de nb1
     }
+
 }
 //Fin fonction
 
@@ -27,6 +28,7 @@ function bouton2(){
         myElement.innerHTML = nb2;
         // on écrit a la balise b2 la valeur de nb2
     }
+
 }
 //Fin fonction
 
@@ -38,7 +40,7 @@ function bouton3 (){
         var myElement = document.getElementById("b3");
         myElement.innerHTML = nb3;
         // on écrit a la balise b3 la valeur de nb3
-     }
+    }
 }
 //Fin fonction
 
@@ -66,25 +68,33 @@ function stop2 (){
 
 //Debut fonction vote  hand
 function hand(){
-  
+
+    /* si on clique sur vote a main levé et qu'ont est sur vote secret*/
    if(secretVote){
-    var myElement = document.getElementById("sp1");
-    myElement.innerHTML = save;
-    secretVote = false;
+        var myElement1 = document.getElementById("sp1");
+        myElement1.innerHTML = save;
+        var myElement2 = document.getElementById("b1");
+        myElement2.innerHTML = nb1;
+        var myElement3 = document.getElementById("b2");
+        myElement3.innerHTML = nb2;
+        var myElement4 = document.getElementById("b3");
+        myElement4.innerHTML = nb3;
+        /* on récupére les balise <b></b> et on y affiche le nouveau resultat de b1..b3 au cas ou on a eu des vote on étant en mode vote secret */
+        secretVote = false;
     }
 }
 //Fin fonctionZ
 
 //Debut fonction vote secret
 function secret() {
-    
+
     if(secretVote == false){
         save = document.getElementById("sp1").innerHTML;
         var myElement = document.getElementById("sp1");
         myElement.innerHTML = "<i class='fas fa-mask'></i> Vote secret <i class='fas fa-mask'></i> "
         console.log("save");
-    }
-    secretVote=true;    
+        secretVote=true;  
+    }  
 }
 //Fin fonction
 
