274aef9e7bd24601f36bfb6ba371fc3b2bc90efa
David <69092875+davidgeorges@users.noreply.github.com>
2020-12-13 15:22:53 +0100

    Commit New

--- Test HTML-JS/test html-js/page1.js
+++ Test HTML-JS/test html-js/page1.js
@@ -1,107 +1,90 @@
 //Declaration variable et Initialisation
 var vote =false;
+var secretVote=false;
 var nb1=0;
 var nb2=0;
 var nb3=0;
+var save = " ";
 
-var bouton1 = () =>{
+//Debut fonction Pour
+function bouton1(){
     console.log("pour");
     if(vote){
         nb1++;
-        document.querySelector('#b1').innerText = `${nb1}`
+        var myElement = document.getElementById("b1");
+        myElement.innerHTML = nb1;
         // on écrit a la balise b1 la valeur de nb1
     }
 }
+//Fin fonction
 
-var bouton2 = () =>{
+//Debut fonction contre
+function bouton2(){
     console.log("contre");
     if(vote){
         nb2++;
-        document.querySelector('#b2').innerText = `${nb2}`
+        var myElement = document.getElementById("b2");
+        myElement.innerHTML = nb2;
         // on écrit a la balise b2 la valeur de nb2
     }
 }
+//Fin fonction
 
-var bouton3 = () =>{
+//Debut fonction absentention
+function bouton3 (){
     console.log("absentention");
     if(vote){
         nb3++;
-        document.querySelector('#b3').innerText = `${nb3}`
+        var myElement = document.getElementById("b3");
+        myElement.innerHTML = nb3;
         // on écrit a la balise b3 la valeur de nb3
      }
 }
+//Fin fonction
 
-var stp1 = () =>{
+//Debut fonction ouvert
+function stop1(){
     console.log("ouvert");
     vote = true;
-    document.querySelector('#v1').innerText = `-vote en cours-`
-    document.querySelector('#v2').innerText = `-vote en cours-`
+    var myElement = document.getElementById("v1");
+    myElement.innerHTML = "-vote en cours-";
+    var myElement2 = document.getElementById("v2");
+    myElement2.innerHTML = "-vote en cours-";
 }
+//Fin fonction
 
-var stp2 = () =>{
+//Debut fonction fermé
+function stop2 (){
     console.log("fermé");
     vote = false;
-    document.querySelector('#v1').innerText = `-vote fermé-`
-    document.querySelector('#v2').innerText = `-vote fermé-`
+    var myElement = document.getElementById("v1");
+    myElement.innerHTML = "-vote fermé-";
+    var myElement2 = document.getElementById("v2");
+    myElement2.innerHTML = "-vote fermé-";
 }
+//Fin fonction
 
-/*
-// si vote = false on ecrit vote fermé  
-if(vote == 0){
-    //écrire²   
-    document.querySelector('#v1').innerText = "-vote fermé -";
-    document.querySelector('#v2').innerText = "-vote fermé -";
+//Debut fonction vote  hand
+function hand(){
+  
+   if(secretVote){
+    var myElement = document.getElementById("sp1");
+    myElement.innerHTML = save;
+    secretVote = false;
+    }
 }
+//Fin fonctionZ
 
-// Fonctioner voter qui prend la fonction onclick et qui fait un code selon le bouton cliquer
-let voter= () =>{
-    // des qu'on clique sur (pour)
-    document.getElementById('bt1').onclick = () => {
-        if(vote){
-            nb1++;
-            document.querySelector('#b1').innerText = `${nb1}`
-            // on écrit a la balise b1 la valeur de nb1
-        }
-     };
-     
-     // des qu'on clique sur (contre)
-     document.getElementById('bt2').onclick = () => {
-        if(vote){
-            nb2++;
-            document.querySelector('#b2').innerText = `${nb2}`
-            // on écrit a la balise b2 la valeur de nb2
-        }
+//Debut fonction vote secret
+function secret() {
     
-     };
-     
-     // des qu'on clique sur (blanc)
-     document.getElementById('bt3').onclick = () => {
-        if(vote){
-        nb3++;
-            document.querySelector('#b3').innerText = `${nb3}`
-            // on écrit a la balise b3 la valeur de nb3
-        }
-     };
-     
-     // si vote = true on ecrit vote ouvert
-     document.getElementById('stp1').onclick = () => {
-        vote = true;
-        document.querySelector('#v1').innerText = `-vote en cours -`
-        document.querySelector('#v2').innerText = `-vote en cours -`
-        // on écrit a la balise v1 et v2 la valeur vote en cours
-        
-     };
-    
-     // si vote = false on ecrit vote fermé
-     document.getElementById('stp2').onclick = () => {
-        vote = false;
-        document.querySelector('#v1').innerText = `-vote fermé -`
-        document.querySelector('#v2').innerText = `-vote fermé -`
-        // on écrit a la balise v1 et v2 la valeur vote fermé
-     };
-    };
+    if(secretVote == false){
+        save = document.getElementById("sp1").innerHTML;
+        var myElement = document.getElementById("sp1");
+        myElement.innerHTML = "<i class='fas fa-mask'></i> Vote secret <i class='fas fa-mask'></i> "
+        console.log("save");
+    }
+    secretVote=true;    
+}
+//Fin fonction
 
-
-
-voter();// appel de la fonction
-*/
