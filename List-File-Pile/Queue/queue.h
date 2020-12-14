f187a216dc93f7cfc0e94728878000443f90ac80
David <69092875+davidgeorges@users.noreply.github.com>
2020-12-14 12:22:14 +0100

    New

---
+++ List-File-Pile/Queue/queue.h
@@ -0,0 +1,52 @@
+#ifndef __QUEUE__H__
+#define __QUEUE__H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ----------------- FILES ---------------- */
+
+/*Définition type Boolean : première valeur prend 0 et la deuxième prend...*/
+
+/*Structure de controle ( FILES ) */
+typedef struct Queue Queue;
+struct Queue
+{
+    /* un element de file va contenir  une valeur  
+    et un pointeur next vers un autre element */
+    int value;
+    Queue *next;
+
+};
+
+
+typedef struct First First;
+struct First
+{
+    /* un element de file va contenir  une valeur  
+    et un pointeur next vers un autre element */
+    int value;
+    Queue *next;
+
+};
+
+
+
+/*Structure de controle  premier element*/
+
+
+static int nb_elements =0;
+
+
+
+/* Prototypes */
+int is_empty_queue(Queue *);
+Queue *new_list(void);
+int queue_length();
+int queue_first(Queue *qu);
+Queue *push_queue(Queue *qu, int x);
+void print_queue(Queue *);
+Queue *pop_queue(Queue *);
+Queue *del_queue(Queue *);
+
+#endif
---
+++ List-File-Pile/Stack/stack.h
@@ -0,0 +1,30 @@
+#ifndef __STACK__H__
+#define __STACK__H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ----------------- PILE ---------------- */
+
+/*Structure de controle ( PILE ) */
+typedef struct Stack Stack;
+struct Stack
+{
+    /* un element de file va contenir  une valeur  
+    et un pointeur next vers un autre element */
+    int value;
+    Stack *next;
+
+};
+
+Stack *new_stack();
+int is_empty_stack(Stack *st);
+Stack *push_stack(Stack *st,int x);
+Stack *clear_stack(Stack *st);
+void print_stack(Stack *st);
+Stack *pop_stack(Stack *st);
+int top_stack(Stack *st);
+int lenght_stack(Stack *st);
+
+
+#endif
