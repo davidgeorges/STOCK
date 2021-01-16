c8730831cfb898a98d2ad3f00e005fc61cb68183
David <69092875+davidgeorges@users.noreply.github.com>
2021-01-16 03:52:47 +0100

    16/01

--- LangageC/ExChaineCaractere/Exo5/Exo5.c
+++ LangageC/ExChaineCaractere/Exo5/Exo5.c
@@ -2,32 +2,49 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TAILLE 20
+#define TAILLE 50
+#define TAILLE2 26
+
+int k =0;
 
 /*Prototype Fonction*/
 int tailleChaine(char tab[]);
-void  compterOccurence(char nom[],int);
+void compterOccurence(char nom[],int,int,int tab[],int);
 void initTab(int tab[]);
-void trouverCarac(char nom);
+
 
 int main(int argc, char const *argv[])
 {
     /*Declaration variable*/
     char nom[TAILLE]=" ";
-    char nom2[TAILLE]=" ";
+    char nom2[]="abcdefghijklmnopqrstuvwxyz";
+    int tab[TAILLE2];
     int taille;
+    char car=96;
 
-    /*Saisie avec affichage*/
+    /*Saisie avec affichage
     printf("Veuillez saisire votre premiere chaine de caractere : ");
-    fgets(nom,TAILLE,stdin);
+    fgets(nom,TAILLE,stdin);*/
 
     /*Appel fonction pour avoir la taille de la chaine */
-    taille = tailleChaine(nom);
-    /*affichage avant appel fonction inverseChaine*/
-    printf("Avant appel fonction : %s\n",nom);
+    taille = tailleChaine(nom2);
+    /*affichage avant appel fonction inverseChaine
+    printf("Avant appel fonction : %s\n",nom);*/
+
+    initTab(tab);
 
     /*Appel fonction inverser la chaine avec affichage apres*/
-    compterOccurence(nom,taille);
+    compterOccurence(nom2,97,0,tab,taille);
+
+
+    printf("Avant appel fonction : %s\n",nom2);
+
+    for (int i = 0; i < TAILLE2; i++)
+    {   
+        car++;
+        printf("Element '%c' trouver [%d] fois \n",car,tab[i]);
+    }
+    
 
 
     return 0;   
@@ -61,27 +78,37 @@ int tailleChaine(char tab[])
 
 
 /*Fonction pour inverser une chaine de caractere*/
-void compterOccurence(char nom[],int taille){
-   
+void compterOccurence(char nom[],int carTrouver,int comptTab2,int tab[],int taille){
     /*Declaration variable*/
-    int tab[26];
-
-    /*Appel fonction initTab*/
-    initTab(tab);
-
-    for (int i = 0; i <taille; i++)
+    int i =0;
+    
+    /*si le compteur allant jusqu'a 26 atteint (25) on arrete la fonction avec le return*/
+    if (comptTab2<TAILLE2)
     {
+        /*on compare le caractere a trouver avec notre tableau*/
+        while (i<taille)
+        {   /*si on a trouver l'element*/
+            if (nom[i] == carTrouver)
+            {
+                tab[comptTab2]++;
+                //printf("compt : %d\n",comptTab2);
+            }
+            i++;
+        }
         
-        
+        /*on rappel la fonction avec un nouveau caractere a trouver et on incrementant le compteur pour effectuer 25 iterrations*/
+        compterOccurence(nom,carTrouver+1,comptTab2+1,tab,taille);
+    }else
+    {
+        return;
     }
     
-
 }
 
 /*Fonction initialiser tableau a 0*/
 void initTab(int tab[]){
     
-    for (int i = 0; i <26; i++)
+    for (int i = 0; i <TAILLE2; i++)
     {
         tab[i] =0;
     }
@@ -89,7 +116,3 @@ void initTab(int tab[]){
 }
 
 
-/*Fonction pour trouver un caractere*/
-void trouverCarac(char nom[]){
-
-}
